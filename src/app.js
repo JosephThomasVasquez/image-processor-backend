eb7ebf6bd7780e5beb1836ca7bdc8c48d0194dd1
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-04-13 17:25:52 -0700

    Added error handler and notfound handler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 
 const app = express();
 
@@ -14,5 +16,9 @@ app.use(
   })
 );
 app.use(express.json());
+
+// error handler
+app.use(errorHandler);
+app.use(notFound);
 
 module.exports = app;
